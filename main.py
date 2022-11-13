d3da4ddb783798fb826813778ec47fd1317eb361
M1keCodingProjects <mikif2002@gmail.com>
2022-11-13 22:05:38 +0100

    Cleared up clutter and bad error-handling.

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 import sys
 from interpreter import Interpreter
 
+sys.tracebacklimit = 0
+
 def main():
-    fileLines : list = readFile()
+    fileLines = readFile()
     interpreter = Interpreter(fileLines, toggle_dbgMode = True)
-    interpreter.exec()
+    interpreter.build()
+    if input("wish to run latest build (y|n)? ").lower() in ("y", "yes"): interpreter.run()
 
 def readFile():
-    try: fileName : str = f"./FILES/{sys.argv[1]}.sudo"
-    except: print("Error: Specify target filename to interpret.")
+    if len(sys.argv) < 2: raise Exception("Missing filename, specify target filename to interpret.")
+    fileName = f"./FILES/{sys.argv[1]}.sudo"
     
-    try:
-        with open(fileName) as fd: return fd.read()
-    except: print("Error: Couldn't open file: invalid filename or file not found, do not include path informations or file extensions.")
+    try: return open(fileName).read()
+    except Exception as e: raise Exception("Couldn't open file: invalid filename or file not found, do not include path informations or file extensions.") from None
 
 if __name__ == "__main__":
     main()
